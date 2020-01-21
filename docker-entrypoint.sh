c05cca89ed7831c4f90dde5d64b6cadf8e0d8154
Mauricio Nunes <mutila@gmail.com>
2020-01-21 13:44:44 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@
 #	CRYPTED_PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $FTP_PASSWORD)
 #	mkdir /home/$FTP_USERNAME
 #	useradd --shell /bin/sh -d /home/$FTP_USERNAME --password $CRYPTED_PASSWORD $FTP_USERNAME
-    chown -R www-data:www-data /var/www/html
+    chown -R www-data:www-data /var/www/html/*/
 #fi
 
 exec "$@"
